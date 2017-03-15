755d30ea0e6fc4c45a9cca07432b83fc8802ca29
Dmytro Orlov <orlov.dm@gmail.com>
2017-03-15 16:53:29 +0100

    add allocate on stack

--- src/main/scala/example/HelloWorld.scala
+++ src/main/scala/example/HelloWorld.scala
@@ -1,10 +1,20 @@
 package example
 
 import scalanative.native._
+import scalanative.native.stdio._
 
 object HelloWorld extends App {
   println("123")
   // CString is an alias to Ptr[CChar]
   val msg: CString = c"Hello, world!\n"
-  stdio.printf(msg)
+  printf(msg)
+
+  type Vec = CStruct3[Double, Double, Double]
+
+  val vec = stackalloc[Vec] // allocate c struct on stack
+  !vec._1 = 10.0            // initialize fields
+  !vec._2 = 20.0
+  !vec._3 = 30.0
+  printf(c"(%.2f %.2f %.2f)\n", !vec._1, !vec._2, !vec._3)
+  println(s"${!vec._1} ${!vec._2} ${!vec._3}")
 }
